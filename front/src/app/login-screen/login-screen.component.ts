e3a3fbbd67bfdfd7a620ef4b67cebb91f6a32f38
Benhur Feld <benhurfeld@gmail.com>
2023-11-24 13:49:26 -0300

    add front and add login in front

---
+++ front/src/app/login-screen/login-screen.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-screen',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-screen.component.html',
+  styleUrl: './login-screen.component.css'
+})
+export class LoginScreenComponent {
+
+}
